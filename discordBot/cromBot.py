91928574149a5b5573022bcb415e74732566467f
박정훈 <ggongjja0@naver.com>
2024-08-07 18:45:03 +0900

    feat: bot command 추가

--- discordBot/cromBot.py
+++ discordBot/cromBot.py
@@ -22,12 +22,12 @@ async def on_ready():
 
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="Help", description="!공지 \n 의미없는 내용 \n\n !채널설정\n 해당 메세지 채널을 봇 작동 채널로 설정\n\n !계산\n 크롬계산기 실행")
+    embed = discord.Embed(title="Help", description="!공지 \n 의미없는 내용 \n\n !채널설정\n 해당 메세지 채널을 봇 작동 채널로 설정\n\n !계산\n 크롬계산기 실행 \n\n !경로\n 각 난이도별 경로 설명")
     await ctx.send(embed = embed)
 
 @bot.command()
 async def 공지(ctx) :
-    embed = discord.Embed(title = "공지", description = "!공지 \n 의미없는 내용 \n\n !채널설정\n 해당 메세지 채널을 봇 작동 채널로 설정\n\n !계산\n 크롬계산기 실행", color = 0x62c1cc)
+    embed = discord.Embed(title = "공지", description = "!공지 \n 의미없는 내용 \n\n !채널설정\n 해당 메세지 채널을 봇 작동 채널로 설정\n\n !계산\n 크롬계산기 실행\n\n !경로\n 각 난이도별 경로 설명", color = 0x62c1cc)
     await ctx.send(embed = embed)
 
 @bot.command()
@@ -37,10 +37,15 @@ async def 채널설정(ctx):
     channel_id = ctx.channel.id
     await ctx.send(f"타겟 채널이 {ctx.channel.mention}으로 설정되었습니다.")
 
+bot.command()
+async def 경로(ctx):
+    embed = discord.Embed(title="난이도 별 경로 안내", description="[30 길]\n 초 -> 초 -> 초 -> 초(무시) -> 노 -> 빨 -> 빨 -> 빨(악령 방) -> 빨 \n\n [50 길]\n 빨 -> 초 -> 빨 -> 초(무시) -> 초 -> 빨-> 빨(악령 방) -> 빨(추격자 20%) -> 이루샤 -> 빨 \n 무조건 왼쪽으로 이동 \n\n [100 길]\n 빨 -> 빨(추격자 28% 추천) -> 노 -> 빨(다잡기) -> 빨 -> 빨-> 빨(악령 방) -> 빨(추격자 48%) -> 이루샤 -> 빨  \n\n")
+    await ctx.send(embed=embed)
+
 @bot.command()
 async def 계산(ctx):
     # 사용자가 계산식을 입력하도록 요청
-    await ctx.send("계산식을 입력하세요 (예: 1 2 3 + + 바스)\n모든 조잡한 메모 내용은 띄워쓰기 해주세요")
+    await ctx.send("계산식을 입력하세요 (예: 1 2 3 + + 바스)\n모든 조잡한 메모 내용은 띄워쓰기 해주세요\n결과를 세번 클릭하면 한번에 선택 가능")
 
     # 사용자로부터 메시지 입력을 기다림
     def check(message):
@@ -70,6 +75,9 @@ async def 계산(ctx):
                 result -= nums[i]
             elif calc[i] == "*":
                 result *= nums[i]
+            else:
+                result = "입력이 잘못되었습니다."
+                break
 
         result_message = f"{result}{word}"
         if word not in ["바스", "벨테인", "루나사", "삼하인", "임볼릭"]:
